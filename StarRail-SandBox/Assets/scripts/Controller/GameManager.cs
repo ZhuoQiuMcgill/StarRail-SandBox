2101a7a87e252c286d176e4c537a40bbd4dc8eef
ZhuoQiuMcgill <QIUZHUOWORK@gmail.com>
2023-08-07 22:57:12 -0400

    PathToRect

--- StarRail-SandBox/Assets/scripts/Controller/GameManager.cs
+++ StarRail-SandBox/Assets/scripts/Controller/GameManager.cs
@@ -37,6 +37,40 @@ public class GameManager : MonoBehaviour
 
     }
 
+    private GameObject PathToRect(Path path)
+    {
+        Vector2 pos1 = path.star1.pos;
+        Vector2 pos2 = path.star2.pos;
+
+        // 计算中点
+        Vector2 midpoint = (pos1 + pos2) / 2;
+
+        // 计算长度
+        float length = Vector2.Distance(pos1, pos2);
+
+        // 计算旋转角度
+        float angleRad = Mathf.Atan2(pos2.y - pos1.y, pos2.x - pos1.x);
+        float angleDeg = angleRad * Mathf.Rad2Deg;
+
+        // 创建GameObject，并以两个星系的ID为名
+        string rectangleName = $"{path.star1.id}-{path.star2.id}";
+        GameObject rectangleObj = new GameObject(rectangleName);
+        var rectTransform = rectangleObj.AddComponent<RectTransform>();
+
+        // 假设宽度为0.5，可以根据需要更改
+        float width = 0.5f;
+
+        rectTransform.sizeDelta = new Vector2(length, width);
+        rectTransform.position = midpoint;
+        rectTransform.rotation = Quaternion.Euler(0, 0, angleDeg);
+
+        // 添加一个图像组件并设置其颜色
+        var imageComponent = rectangleObj.AddComponent<Image>();
+        imageComponent.color = Color.white;
+
+        return rectangleObj;
+    }
+
     // TODO: 将已经生产的gameobject渲染至画面
     private void rander()
     {
